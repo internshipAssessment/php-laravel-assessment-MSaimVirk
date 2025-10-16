95f2ff96057c80ad4d34bb409c31aef9e6af6c37
MSaimVirk <saim123sadiq@gmail.com>
2025-10-16 03:43:43 -0700

    sql

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -8,13 +8,17 @@
 -- Return: user_id, name, order_count
 -- Sort: order_count DESC
 -- TODO: Write the SQL below
-SELECT
+--SELECT
   -- TODO
+  select user_id, name, order_count
+  from (select count(orders) from users join orders where id=user_id
+        group by user_id ) 
 ;
 
 -- Task 2 — Products never ordered
 -- Return: id, name of products that appear in ZERO order_items
 -- TODO: Write the SQL below
-SELECT
+SELECT name, id from (products join order_items where id=product_id)
+where quantity='empty' or null
   -- TODO
 ;
